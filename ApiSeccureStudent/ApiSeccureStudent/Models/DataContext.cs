10b05c3e691b7b30d68743e99bc4dcceb037a11e
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-08-29 10:12:55 -0400

    Con controlador de Students mas autorizacion

---
+++ ApiSeccureStudent/ApiSeccureStudent/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ApiSeccureStudent.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiSeccureStudent.Models.Student> Students { get; set; }
+    }
+}
---
+++ ApiSeccureStudent/ApiSeccureStudent/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ApiSeccureStudent.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        [Range(1, 9999)]
+        public int StudentID { get; set; }
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string LastName { get; set; }
+
+        [StringLength(30, ErrorMessage = "The field {0} must contein between {2} and {1} characteres", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
