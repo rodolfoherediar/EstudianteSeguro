1b14fc2ff1e1e7d6fe12d531f62d6f9659fb7311
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-09-05 12:02:53 -0400

    Se agrego el proyecto administracion mvc

---
+++ ApiSeccureStudent/admStudentSecurity/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace admStudentSecurity.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+       
+        public int StudentID { get; set; }
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string LastName { get; set; }
+
+        [StringLength(30, ErrorMessage = "The field {0} must contein between {2} and {1} characteres", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
