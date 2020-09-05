e1635e1a30e8d0ddcc070e3018a65f5572bcc5eb
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-09-05 10:29:25 -0400

    Se agrego UnitTest

---
+++ ApiSeccureStudent/UTEstudianteSeguro.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UTEstudianteSeguro.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTEstudianteSeguro.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ddbc9b0-5eed-41de-806d-5806006e385c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ApiSeccureStudent/UTEstudianteSeguro.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using ApiSeccureStudent.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UTEstudianteSeguro.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGetStudent()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController();
+
+            //Act
+            var ListaEstudiante = studentsController.GetStudents();
+
+            //Assert
+            Assert.IsNotNull(ListaEstudiante);
+        }
+    }
+}
